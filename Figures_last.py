7abeab55367c9c21fefb20429fca870aa31ad95e
andrey010 <andr780008@gmail.com>
2022-02-22 21:27:39 +0200

    after review

--- Figures_last.py
+++ Figures_last.py
@@ -1,51 +1,47 @@
 import math
 import time
+from abc import ABC, abstractmethod
 
-class Figures:
-    def __init__(self, width, length):
-        self.width=wid
-        self.length=leng
+class Figures(ABC):
+    
+    @abstractmethod
+    def area(self):
+        pass
 
+#fr = Figures() #try run that line  ---> TypeError: Can't instantiate abstract class Figures with abstract method area it mean 
 
 class Rectangle(Figures):
+    def __init__(self, width, length):        
+        self.width = width
+        self.length = length
+        
     def area(self):
-        return print(int(self.width*self.length))
+        return print(int(self.width * self.length), "rectangle area")
     
 class Circle(Figures):
-    def __init__(self, width, length, radius):
-        super().__init__(width, length)
-        
-        self.radius=rad
+    def __init__(self, radius): #  radius instead  rad       
+        self.radius=radius    
     
-    
-    def area(self):
-        return print(int(math.pi*(rad**2)))
+    def area(self): # if u will use class without area method you will get error --> TypeError: Can't instantiate abstract class Circle with abstract method area
+        return print(int(math.pi * (self.radius ** 2)), "circle area") # self for get method ot variable of current class
     
 class Square(Figures):
-    def __init__(self, width, length, lenght2):
-        super().__init__(width, length, length2)
-        
-        self.lenght2=leng2
+    def __init__(self, width):        
+        self.width = width
     
     def area(self):
-        return print(int(leng2**2))
+        return print(int(self.width ** 2), "square area") # self for get method ot variable of current class
     
+f1=Rectangle(10, 12)
+f1.area()   
 
+f2=Circle(10)
+f2.area()
 
+f3=Square(12)
+f3.area()
 
-fig=input("There is a rectangle, a square, a circle.\nWhat figura area do you need?\n")
-if fig.lower()==str("rectangle"):
-    wid=int(input("Width: "))
-    leng=int(input("Length: "))
-    f1=Rectangle(wid, leng)
-    f1.area()
-    
-elif fig.lower()==str("circle"):
-    rad=int(input("Radius: "))
-    f2=Circle(rad)
-    f2.area()
-
-elif fig.lower()==str("square"):
-    leng2=int(input("Length: "))
-    f3=Square(leng2)
-    f3.area()
+#Please dont write console program, just implement some logic and print
+# 1.You should read about self (this) in classes, what is abstract classes
+# 2. What is function arguments of function
+# 3. Function scope
